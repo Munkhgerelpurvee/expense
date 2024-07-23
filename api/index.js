b5d3c9ca7176d171ace1ba118040c2c175caec01
Munkhgerelpurvee <164706050+Munkhgerelpurvee@users.noreply.github.com>
2024-07-23 12:39:49 +0800

    first commit

---
+++ api/index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+var cors = require('cors');
+
+
+const app = express()
+
+app.use(cors());
+const port = 3001;
+
+app.get('/', (req, res) => {
+  res.json([
+    {
+    
+        id: 1,
+        src: "/Blackcoffee.png",
+        title: "Black Coffee",
+        desc: "Black coffee is a beverage made from roasted coffee beans. The beans are ground and soaked in water, which releases their flavor, color, caffeine content, and nutrients. ",
+      },
+      {
+        id: 2,
+        src: "/Cappuccino.jpg",
+   
+        title: "Cappuccino",
+        desc: "A cappuccino is an espresso-based coffee drink that is traditionally prepared with steamed milk foam (microfoam). Cappuccino. Type, Hot.",
+      },
+      {
+        id: 3,
+        src: "/Espresso.png",
+        title: "Espresso",
+        desc: "Espresso is a concentrated form of coffee, served in shots. It's made of two ingredients - finely ground, 100% coffee, and hot water.",
+      },
+      {
+        id: 4, 
+        src: "/Latte.jpg",
+        title: "Latte",
+        desc: "A latte or caffè latte is a milk coffee that boasts a silky layer of foam as a real highlight to the drink. A true latte will be made up of one or two shots of espresso, steamed milk and a final, thin layer of frothed milk on top",
+      },
+      {
+        id: 5,
+        src: "/Macchiato.jpg",
+        title: "Macchiato",
+        desc: "Caffè macchiato, sometimes called espresso macchiato, is an espresso coffee drink with a small amount of milk, usually foamed. ",
+      },
+      {
+        id: 6,
+        src: "/Latte.jpg",
+        title: "Macchiato",
+        desc: "Caffè macchiato, sometimes called espresso macchiato, is an espresso coffee drink with a small amount of milk, usually foamed. ",
+      },
+  ])
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
