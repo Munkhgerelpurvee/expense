e78d5625343738f19a30c51ac02e02f927615e07
Munkhgerelpurvee <164706050+Munkhgerelpurvee@users.noreply.github.com>
2024-07-29 14:08:05 +0800

    Add category-n Icon-g tatav.

--- fe/src/components/AddCategory.js
+++ fe/src/components/AddCategory.js
@@ -12,6 +12,19 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { AiFillPlusCircle } from "react-icons/ai";
+import { FaHouse } from "react-icons/fa6";
+import { RiArrowDownSFill } from "react-icons/ri";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { Textarea } from "@/components/ui/textarea";
+
 
 
 
@@ -43,27 +56,73 @@ export const AddCategory = () => {
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
+          <DialogTitle>Add Category</DialogTitle>
           <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
+            Make changes 
           </DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
-          <div className="grid items-center grid-cols-4 gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+
+          {/*  */}
+
+        <div className="flex flex-1 border border-solid border-[gray]">
+                    <NavigationMenu>
+                      <NavigationMenuList>
+                        <NavigationMenuItem>
+                          <div className="flex flex-col">
+                            {/* <Label
+                              className="text-[gray] font-light"
+                              htmlFor="r2"
+                            >
+                              Payee
+                            </Label> */}
+                            <NavigationMenuTrigger className="w-full bg-[#E5E7EB] mt-4">
+                              <FaHouse />
+                            </NavigationMenuTrigger>
+                          </div> 
+                        
+                          <NavigationMenuContent>
+                            <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                              <li className="row-span-3">
+                                <NavigationMenuLink asChild>
+                                  <a
+                                    className="flex flex-col justify-end w-full h-full p-6 no-underline rounded-md outline-none select-none bg-gradient-to-b from-muted/50 to-muted focus:shadow-md"
+                                    href="/"
+                                  >
+                                
+                                    <div className="mt-4 mb-2 text-lg font-medium">
+                                      shadcn/ui
+                                    </div>
+                                    <p className="text-sm leading-tight text-muted-foreground">
+                                      Beautifully designed components that you
+                                      can copy and paste into your apps.
+                                      Accessible. Customizable. Open Source.
+                                    </p>
+                                  </a>
+                                </NavigationMenuLink>
+                              </li>
+                            </ul>
+                          </NavigationMenuContent>
+                        </NavigationMenuItem>
+                      </NavigationMenuList>
+                    </NavigationMenu>
+                  </div>
+          {/*  */}
+          <div className="relative flex items-center gap-4">
+           
+            <Input id="name" value=""  placeholder="Name" className="relative col-span-3" />
+            <RiArrowDownSFill className="absolute"/>
+            
           </div>
-          <div className="grid items-center grid-cols-4 gap-4">
+          {/* <div className="grid items-center grid-cols-4 gap-4">
             <Label htmlFor="username" className="text-right">
               Username
             </Label>
             <Input id="username" value="@peduarte" className="col-span-3" />
-          </div>
+          </div> */}
         </div>
         <DialogFooter>
-          <Button type="submit">Save changes</Button>
+          <Button  className = "bg-[#16A34A]"type="submit">Save changes</Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
---
+++ fe/src/components/IconCategory.js
@@ -0,0 +1,86 @@
+import { FaHouse } from "react-icons/fa6";
+import { PiHouseLineFill } from "react-icons/pi";
+import { PiIdentificationBadgeFill } from "react-icons/pi";
+import { PiIdentificationCardFill } from "react-icons/pi";
+import { PiLadderBold } from "react-icons/pi";
+import { PiIntersectSquareFill } from "react-icons/pi";
+import { PiImageSquareFill } from "react-icons/pi";
+import { PiMagnifyingGlassPlusFill } from "react-icons/pi";
+import { BiSolidMicrophone } from "react-icons/bi";
+import { PiMicrosoftExcelLogoFill } from "react-icons/pi";
+import { PiNotepadFill } from "react-icons/pi";
+import { PiListPlusBold } from "react-icons/pi";
+import { FaLeaf } from "react-icons/fa";
+import { PiNumberFiveFill } from "react-icons/pi";
+import { PiNumberCircleSevenFill } from "react-icons/pi";
+import { PiRoadHorizonFill } from "react-icons/pi";
+import { PiHourglassSimpleMediumFill } from "react-icons/pi";
+import { PiAnchorSimpleFill } from "react-icons/pi";
+import { PiBezierCurveFill } from "react-icons/pi";
+import { PiExcludeFill } from "react-icons/pi";
+import { PiVignetteFill } from "react-icons/pi";
+import { IoIosBaseball } from "react-icons/io";
+import { BsFillQuestionCircleFill } from "react-icons/bs";
+import { PiExamFill } from "react-icons/pi";
+import { PiWatchFill } from "react-icons/pi";
+import { PiGlobeSimpleFill } from "react-icons/pi";
+import { PiOrangeSliceFill } from "react-icons/pi";
+import { PiPeaceFill } from "react-icons/pi";
+import { PiToiletPaperFill } from "react-icons/pi";
+import { PiPencilLineFill } from "react-icons/pi";
+
+
+
+
+
+
+
+export const IconCategory = () => {
+
+    const icond = {
+
+        home1: FaHouse,
+        home2:PiHouseLineFill, 
+        IdentificationBadge: PiIdentificationBadgeFill ,
+        IdentificationCard:PiIdentificationCardFill,
+        Ladder:PiLadderBold,
+        IntersectSquare:PiIntersectSquareFill,
+        ImageSquare:PiImageSquareFill,
+        MagnifyingGlassPlus:PiMagnifyingGlassPlusFill,
+        Microphone:BiSolidMicrophone,
+        MicrosoftExcelLogo:PiMicrosoftExcelLogoFill,
+        Notepad:PiNotepadFill,
+        ListPlus:PiListPlusBold,
+        Leaf:FaLeaf,
+        NumberFive:PiNumberFiveFill,
+        NumberCircleSeven:PiNumberCircleSevenFill,
+        RoadHorizon:PiRoadHorizonFill,
+        HourglassSimpleMedium:PiHourglassSimpleMediumFill,
+        AnchorSimple:PiAnchorSimpleFill,
+        BezierCurve:PiBezierCurveFill,
+        Exclude:PiExcludeFill ,
+        Vignette:PiVignetteFill,
+        Baseball:IoIosBaseball,
+        Question:BsFillQuestionCircleFill,
+        Exam:PiExamFill,
+        Watch:PiWatchFill ,
+        GlobeSimple:PiGlobeSimpleFill,
+        OrangeSlice:PiOrangeSliceFill,
+        Peace:PiPeaceFill,
+        ToiletPaper:PiToiletPaperFill,
+        Pencil: PiPencilLineFill,
+
+
+    }
+
+
+
+
+    return(
+
+        <>
+        </>
+    )
+
+
+}
